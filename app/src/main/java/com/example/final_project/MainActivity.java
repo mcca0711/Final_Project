5c4b4ddb68811f0c23b5c21fd4a7fd9a17f16ac4
mcca0711 <mcca0711@algonquinlive.com>
2021-12-10 23:19:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public String user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
